437e28e529e186fb440b45a956b98907b10fbf8c
Itros97 <61584391+Itros97@users.noreply.github.com>
2021-03-19 16:54:04 +0100

    Update prueba.java

--- src/main/java/com/initial/prueba.java
+++ src/main/java/com/initial/prueba.java
@@ -2,6 +2,7 @@ package com.initial;
 
 public class prueba {
     public static void main(String[] args) {
-        System.out.println("Hola, mundo");
+        System.out.println("Hello, world");
     }
 }
+  
