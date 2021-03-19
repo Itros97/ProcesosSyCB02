cb18b499b333b9f2a36577382c7358e79a6e4dec
MikelDela <mikel.delatorre@opendeusto.es>
2021-03-19 16:50:42 +0100

    Ventana Login

---
+++ src/main/java/loginVShop.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class loginVShop extends JFrame {
+
+    private JPanel mainPanel;
+    private JTextField textFieldUsuario;
+    private JPasswordField passwordField;
+    private JLabel labelUsuario;
+    private JLabel labelPassword;
+    private JButton buttonIngresar;
+
+    public loginVShop(){
+        this.setContentPane(mainPanel);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args){
+        JFrame frame = new loginVShop();
+        frame.setVisible(true);
+        frame.setSize(500,500);
+    }
+}
