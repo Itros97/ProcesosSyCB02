7083135d2d326d706ae83e5cafe4ba2eb6c8681d
Itros97 <61584391+Itros97@users.noreply.github.com>
2021-03-22 13:27:55 +0100

    Molde de ventana de administracion de usuarios

---
+++ src/main/java/com/ui/VAdminUser.java
@@ -0,0 +1,10 @@
+package com.ui;
+
+import javax.swing.*;
+
+public class VAdminUser {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JTabbedPane tabbedPane2;
+    private JTabbedPane tabbedPane3;
+}
