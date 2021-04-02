9ce90c126eddc5b0d542a056785dc55915b3a859
MikelDela <mikel.delatorre@opendeusto.es>
2021-04-02 13:42:29 +0200

    Ventana VShop creada

--- src/main/java/com/ui/mainVShop.java
+++ src/main/java/com/ui/mainVShop.java
@@ -2,5 +2,28 @@ package com.ui;
 
 import javax.swing.*;
 
-public class mainVShop {
+public class mainVShop extends JFrame{
+    private JButton tiendaButton;
+    private JButton carritoButton;
+    private JButton historialButton;
+    private JButton añadirCarritoButton;
+    private JPanel buttonPanel;
+    private JPanel mainPanel;
+
+    public mainVShop()
+    {
+        add(mainPanel);
+    }
+
+    public static void main(String[] args){
+        mainVShop window = new mainVShop();
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
+        window.pack();
+        window.setTitle("VShop");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setBounds(100, 100, 1280, 720);
+
+    }
+
 }
