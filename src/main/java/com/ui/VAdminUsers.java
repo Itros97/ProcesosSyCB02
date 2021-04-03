6bc426d05bd01bc70bb0821f89886f24f1ad1788
Itros97 <61584391+Itros97@users.noreply.github.com>
2021-04-03 17:52:49 +0200

    Creada ui completa crear usuario

--- src/main/java/com/ui/VAdminUsers.java
+++ src/main/java/com/ui/VAdminUsers.java
@@ -5,6 +5,14 @@ import javax.swing.*;
 public class VAdminUsers extends JFrame{
     private JTabbedPane tabbedPane1;
     private JPanel MainPanel;
+    private JButton bcrear;
+    private JFormattedTextField ftfapellido2;
+    private JFormattedTextField ftfapellido1;
+    private JFormattedTextField ftfpassword;
+    private JFormattedTextField ftfnombre;
+    private JFormattedTextField ftfcorreo;
+    private JFormattedTextField ftfdireccion;
+    private JFormattedTextField ftftarjeta;
 
     public VAdminUsers()
     {
