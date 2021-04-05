8d3fe274e6cdaaf5160433f66ca8306710059c0d
MikelDela <mikel.delatorre@opendeusto.es>
2021-04-05 19:59:20 +0200

    Interfaz Login (mejorada) y Registro (añadida)

--- src/main/java/com/ui/loginVShop.java
+++ src/main/java/com/ui/loginVShop.java
@@ -5,15 +5,27 @@ import javax.swing.*;
 public class loginVShop extends JFrame {
 
     private JPanel mainPanel;
-    private JTextField textFieldUsuario;
-    private JPasswordField passwordField;
-    private JLabel labelUsuario;
-    private JLabel labelPassword;
-    private JButton buttonIngresar;
+    private JTabbedPane tabbedPane1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton ingresarButton;
+    private JButton bcrear;
+    private JFormattedTextField ftfapellido2;
+    private JFormattedTextField ftfapellido1;
+    private JFormattedTextField ftfpassword;
+    private JFormattedTextField ftfnombre;
+    private JFormattedTextField ftfcorreo;
+    private JFormattedTextField ftfdireccion;
+    private JFormattedTextField ftftarjeta;
 
     public loginVShop(){
+        initialize();
         this.setContentPane(mainPanel);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void initialize(){
+
     }
 
     public static void main(String[] args){
