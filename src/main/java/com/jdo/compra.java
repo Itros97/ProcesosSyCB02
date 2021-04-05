54b7bfa32222a8e8b04ecac29ff20d0d2207ed11
Itros97 <61584391+Itros97@users.noreply.github.com>
2021-04-05 17:58:27 +0200

    cambios recogidos y cambio en un JDO

--- src/main/java/com/jdo/compra.java
+++ src/main/java/com/jdo/compra.java
@@ -11,7 +11,38 @@ public class compra {
     @Persistent(valueStrategy = IdGeneratorStrategy.INCREMENT)
     protected int id;
     //Datos
-
+    protected int us;
+    protected String codename;
     //Key Externas
 
+
+    public compra(int id, int us, String codename) {
+        this.id = id;
+        this.us = us;
+        this.codename = codename;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUs() {
+        return us;
+    }
+
+    public void setUs(int us) {
+        this.us = us;
+    }
+
+    public String getCodename() {
+        return codename;
+    }
+
+    public void setCodename(String codename) {
+        this.codename = codename;
+    }
 }
