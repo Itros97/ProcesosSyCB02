3b70bda3b51f7df3d15700e763057f27f12ad9b3
mikelgomez <mikelgomez@opendeusto.es>
2021-04-06 20:08:00 +0200

    Añadidas claves externas junto con respectivos metodos

--- src/main/java/com/jdo/Compra.java
+++ src/main/java/com/jdo/Compra.java
@@ -1,17 +1,54 @@
 package com.jdo;
 
-import javax.jdo.annotations.IdGeneratorStrategy;
-import javax.jdo.annotations.PersistenceCapable;
-import javax.jdo.annotations.Persistent;
-import javax.jdo.annotations.PrimaryKey;
+import javax.jdo.annotations.*;
 
 @PersistenceCapable
 public class Compra {
     @PrimaryKey
     @Persistent(valueStrategy = IdGeneratorStrategy.INCREMENT)
-    protected int id;
+    protected int idCompra;
     //Datos
 
     //Key Externas
+    @ForeignKey
+    protected int idUsuarioCompra;
 
+    @ForeignKey
+    protected int idProductoCompra;
+
+    public Compra(int idCompra, int idUsuarioCompra, int idProductoCompra) {
+        this.idCompra = idCompra;
+        this.idUsuarioCompra = idUsuarioCompra;
+        this.idProductoCompra = idProductoCompra;
+    }
+
+    public Compra() {
+        this.idCompra = 0;
+        this.idUsuarioCompra = 0;
+        this.idProductoCompra = 0;
+    }
+
+    public int getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(int idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public int getIdUsuarioCompra() {
+        return idUsuarioCompra;
+    }
+
+    public void setIdUsuarioCompra(int idUsuarioCompra) {
+        this.idUsuarioCompra = idUsuarioCompra;
+    }
+
+    public int getIdProductoCompra() {
+        return idProductoCompra;
+    }
+
+    public void setIdProductoCompra(int idProductoCompra) {
+        this.idProductoCompra = idProductoCompra;
+    }
 }
