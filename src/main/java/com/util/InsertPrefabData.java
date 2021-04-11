33c9498420807519ad70496be0aeeb9372abbac5
Itros97 <61584391+Itros97@users.noreply.github.com>
2021-04-11 13:56:49 +0200

    Insertar Producto

--- src/main/java/com/util/InsertPrefabData.java
+++ src/main/java/com/util/InsertPrefabData.java
@@ -7,6 +7,7 @@ public class InsertPrefabData {
     public static void main(String[] args) {
         ConexionDB bd = new ConexionDB();
         //bd.ReiniciarBasesDeDatos();
+        
         Producto p1 = new Producto("hey","dsdsds",5,"Hola");
         bd.InsertarProducto(p1);
 
