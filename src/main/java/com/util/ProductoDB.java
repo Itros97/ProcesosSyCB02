75a2d3cd345ac742923af14d241b1fab19f05036
MikelDela <mikel.delatorre@opendeusto.es>
2021-04-20 12:41:29 +0200

    Update ProductoDB.java

--- src/main/java/com/util/ProductoDB.java
+++ src/main/java/com/util/ProductoDB.java
@@ -122,7 +122,7 @@ public class ProductoDB {
 
 
     //ELIMINAR PRODUCTO
-    public static void eliminarProducto(String nombre) {
+    public static void eliminarProducto(int id) {
 
         PreparedStatement preparedStatement= null;
         Connection con = ConexionDB.Conexion();
