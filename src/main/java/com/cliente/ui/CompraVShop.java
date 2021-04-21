678470f22ae42afc0c0554114eadec24bf45afb7
Itros97 <61584391+Itros97@users.noreply.github.com>
2021-04-21 15:23:49 +0200

    Cambios menores en la ui

--- src/main/java/com/cliente/ui/CompraVShop.java
+++ src/main/java/com/cliente/ui/CompraVShop.java
@@ -1,12 +1,4 @@
 package com.cliente.ui;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.sql.Connection;
-
-
-public class CompraVShop extends JFrame {
-    private JPanel compraMainPanel;
+public class CompraVShop {
 }
