5a4442cf1b6fa92bbd7b070fc28c0980239df965
diegopresa <diego.presa@opendeusto.es>
2021-04-27 17:53:28 +0200

    Creada la interfaz historial y anadidas varias funcionalidades

---
+++ src/main/java/com/cliente/ui/HistorialVShop.java
@@ -0,0 +1,47 @@
+package com.cliente.ui;
+
+        import javax.swing.*;
+        import java.awt.event.ActionEvent;
+        import java.awt.event.ActionListener;
+        import java.awt.event.MouseEvent;
+
+public class HistorialVShop extends JFrame {
+    private JPanel mainPanel;
+    private JTable table;
+    private JTable table1;
+    private JButton cargarCompras;
+
+
+    public HistorialVShop(){
+        initialize();
+        add(mainPanel);
+
+        table.addMouseListener(new java.awt.event.MouseAdapter(){
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+    }
+    public void initialize(){
+        cargarCompras.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //Falta insertar compra
+            }
+        });
+
+    }
+
+    public static void main(String[] args) {
+        HistorialVShop window = new HistorialVShop();
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
+        window.pack();
+        window.setTitle("Historial");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setBounds(100, 100, 1000, 600);
+
+    }
+}
