ef349dc0c283f48408942c1ea2a5d2d4a88cf3b5
mikelgomez <mikelgomez@opendeusto.es>
2021-04-27 13:00:25 +0200

    Modificada dimensiones ventana e implementado el ticket en txt

--- src/main/java/com/server/Main.java
+++ src/main/java/com/server/Main.java
@@ -1,4 +1,4 @@
-package com.server;
+/*package com.server;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
@@ -6,11 +6,13 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 import java.io.IOException;
 import java.net.URI;
-
+ */
 /**
  * Main class.
  *
  */
+
+/*
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
     public static final String BASE_URI = "http://localhost:8080/myapp/";
@@ -18,7 +20,7 @@ public class Main {
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
      * @return Grizzly HTTP server.
-     */
+
     public static HttpServer startServer() {
         // create a resource config that scans for JAX-RS resources and providers
         // in com.initial package
@@ -27,13 +29,14 @@ public class Main {
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
-    }
+    } */
 
     /**
      * Main method.
      * @param args
      * @throws IOException
      */
+    /*
     public static void main(String[] args) throws IOException {
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
@@ -41,5 +44,5 @@ public class Main {
         System.in.read();
         server.stop();
     }
-}
+} */
 
