d5851e1ad155b6f6b30426505fb44485230f6a6c
Itros97 <61584391+Itros97@users.noreply.github.com>
2021-04-30 14:07:19 +0200

    Problemas de lanzamiento de app desde cmd solucionados

--- src/main/java/com/database/UsuarioDB.java
+++ src/main/java/com/database/UsuarioDB.java
@@ -1,7 +1,7 @@
 package com.database;
 
 import com.cliente.jdo.Usuario;
-import com.cliente.ui.MainVShop;
+
 
 
 import javax.swing.*;
@@ -148,11 +148,12 @@ public class UsuarioDB {
         }
         if (comprobar == true) {
             System.out.println("Existe y la contrasenya concuerda,permitir el logeo");
-
+            /*
             com.cliente.ui.MainVShop window = new com.cliente.ui.MainVShop();
             window.setVisible(true);
             window.setTitle("Tienda");
-            window.setBounds(100, 100, 1280, 720);
+        	window.setBounds(100, 100, 1280, 720);
+         */   
 
         } else {
             System.out.println("Problema de conexion");
