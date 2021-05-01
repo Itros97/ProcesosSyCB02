dd1d658b226af55c10672d304dd6f71573a338a3
MikelDela <mikel.delatorre@opendeusto.es>
2021-05-01 17:22:27 +0200

    CarritoDBTest creado

---
+++ src/test/java/com/database/CarritoDBTest.java
@@ -0,0 +1,37 @@
+package com.database;
+
+import static org.junit.Assert.*;
+
+import java.io.FileInputStream;
+
+import org.dbunit.PropertiesBasedJdbcDatabaseTester;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.junit.Test;
+
+public class CarritoDBTest {
+/*
+	public CarritoDBTest() {
+
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "com.mysql.jdbc.Driver");
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, "jdbc:mysql://localhost/EStoreDB");
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "spq");
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "spq");
+	}
+
+	protected IDataSet getDataSet() throws Exception {
+		return new FlatXmlDataSetBuilder().build(new FileInputStream("dbsample/carrito.xml"));
+	}
+
+	@Test
+	public void testInsertarCarrito() {
+		assertEquals(false, false);
+	}
+	
+	@Test
+	public void testDeleteCarrito() {
+		assertEquals(false, false);
+	}
+*/
+
+}
