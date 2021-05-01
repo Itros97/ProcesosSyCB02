804da33fa4ab5541831f5bf6941d6907af1bc39b
diegopresa <diego.presa@opendeusto.es>
2021-05-01 14:02:38 +0200

    Acabada interfaz de modificar usuarios antigua

--- src/main/java/com/cliente/ui/AdminUsersVShop.java
+++ src/main/java/com/cliente/ui/AdminUsersVShop.java
@@ -30,6 +30,14 @@ public class AdminUsersVShop {
 	private JTextField ape1;
 	private JTextField ape2;
 	private JPasswordField password;
+	private JTextField correo11;
+	private JTextField nombre1;
+	private JTextField dir1;
+	private JTextField tarjeta1;
+	private JTextField nickname1;
+	private JTextField ape11;
+	private JTextField ape21;
+	private JPasswordField password1;
 
 	/**
 	 * Launch the application.
@@ -196,15 +204,85 @@ public class AdminUsersVShop {
 		tabbedPane.addTab("Modificar", null, panelModificar, null);
 		panelModificar.setLayout(null);
 		
-		JButton btnNewButton = new JButton("MODIFICAR USUARIO");
-		btnNewButton.addActionListener(new ActionListener() {
+		JLabel labelNombre1 = new JLabel("Nombre");
+		labelNombre1.setBounds(10, 11, 93, 24);
+		panelModificar.add(labelNombre1);
+		
+		JLabel labelCorreo1 = new JLabel("Correo");
+		labelCorreo1.setBounds(256, 16, 93, 24);
+		panelModificar.add(labelCorreo1);
+		
+		JLabel labelContrasenya1 = new JLabel("Contraseña");
+		labelContrasenya1.setBounds(10, 72, 93, 24);
+		panelModificar.add(labelContrasenya1);
+		
+		JLabel labelDireccion1 = new JLabel("Direccion");
+		labelDireccion1.setBounds(256, 77, 93, 24);
+		panelModificar.add(labelDireccion1);
+		
+		JLabel labelApe11 = new JLabel("Apellido 1");
+		labelApe11.setBounds(10, 139, 93, 24);
+		panelModificar.add(labelApe11);
+		
+		JLabel labelApe21 = new JLabel("Apellido 2");
+		labelApe21.setBounds(10, 211, 93, 24);
+		panelModificar.add(labelApe21);
+		
+		JLabel labelTarjeta1 = new JLabel("Tarjeta Credito");
+		labelTarjeta1.setBounds(256, 139, 93, 24);
+		panelModificar.add(labelTarjeta1);
+		
+		JLabel labelNickname1 = new JLabel("Nickname");
+		labelNickname1.setBounds(256, 211, 93, 24);
+		panelModificar.add(labelNickname1);
+		
+		nombre1 = new JTextField();
+		nombre1.setBounds(10, 41, 213, 20);
+		panelModificar.add(nombre1);
+		nombre1.setColumns(10);
+		
+		correo11 = new JTextField();
+		correo11.setColumns(10);
+		correo11.setBounds(256, 41, 213, 20);
+		panelModificar.add(correo11);
+		
+		dir1 = new JTextField();
+		dir1.setColumns(10);
+		dir1.setBounds(256, 108, 213, 20);
+		panelModificar.add(dir1);
+		
+		tarjeta1 = new JTextField();
+		tarjeta1.setColumns(10);
+		tarjeta1.setBounds(256, 174, 213, 20);
+		panelModificar.add(tarjeta1);
+		
+		nickname1 = new JTextField();
+		nickname1.setColumns(10);
+		nickname1.setBounds(256, 246, 213, 20);
+		panelModificar.add(nickname1);
+		
+		ape11 = new JTextField();
+		ape11.setColumns(10);
+		ape11.setBounds(10, 174, 213, 20);
+		panelModificar.add(ape11);
+		
+		ape21 = new JTextField();
+		ape21.setColumns(10);
+		ape21.setBounds(10, 246, 213, 20);
+		panelModificar.add(ape21);
+		
+		password1 = new JPasswordField();
+		password1.setBounds(10, 108, 213, 20);
+		panelModificar.add(password1);
+		
+		JButton botonModificarUsuario = new JButton("MODIFICAR USUARIO");
+		botonModificarUsuario.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				//Crear update en UsuarioDB para modificar y actualizar usuarios
 			}
 		});
-		btnNewButton.setBounds(10, 278, 456, 23);
-		panelModificar.add(btnNewButton);
-		
+		botonModificarUsuario.setBounds(10, 280, 459, 23);
+		panelModificar.add(botonModificarUsuario);
 		
 	}
 }
