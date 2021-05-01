b056e6c50d03d6cd32b7dcb987da15bce7ccf477
diegopresa <diego.presa@opendeusto.es>
2021-05-01 14:08:52 +0200

    Actualizada interfaz de historial de compras

--- src/main/java/com/cliente/ui/HistorialVShop.java
+++ src/main/java/com/cliente/ui/HistorialVShop.java
@@ -3,10 +3,17 @@ package com.cliente.ui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class HistorialVShop {
 
 	private JFrame frame;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -38,54 +45,23 @@ public class HistorialVShop {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JList list = new JList();
+		list.setBounds(10, 11, 207, 239);
+		frame.getContentPane().add(list);
+		
+		table = new JTable();
+		table.setBounds(227, 215, 197, -202);
+		frame.getContentPane().add(table);
+		
+		JButton cargarCompras = new JButton("Cargar Compras");
+		cargarCompras.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//Falta insertar las compras desde la base de datos e insertarlas en la lista
+			}
+		});
+		cargarCompras.setBounds(227, 227, 200, 23);
+		frame.getContentPane().add(cargarCompras);
 	}
-
 }
-
-//package com.cliente.ui;
-//
-//import javax.swing.*;
-//import java.awt.event.ActionEvent;
-//import java.awt.event.ActionListener;
-//import java.awt.event.MouseEvent;
-//
-//public class HistorialVShop extends JFrame {
-//private JPanel mainPanel;
-//private JTable table;
-//private JTable table1;
-//private JButton cargarCompras;
-//
-//
-//public HistorialVShop(){
-//initialize();
-//add(mainPanel);
-//
-//table.addMouseListener(new java.awt.event.MouseAdapter(){
-//    @Override
-//    public void mouseClicked(MouseEvent e) {
-//        super.mouseClicked(e);
-//    }
-//});
-//}
-//public void initialize(){
-//cargarCompras.addActionListener(new ActionListener() {
-//    @Override
-//    public void actionPerformed(ActionEvent e) {
-//
-//        //Falta insertar compra
-//    }
-//});
-//
-//}
-//
-//public static void main(String[] args) {
-//HistorialVShop window = new HistorialVShop();
-//window.setVisible(true);
-//window.setLocationRelativeTo(null);
-//window.pack();
-//window.setTitle("Historial");
-//window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//window.setBounds(100, 100, 1000, 600);
-//
-//}
-//}
