62ed57891b10ddd2dfb4d933c26aa2ffa42db411
MikelDela <mikel.delatorre@opendeusto.es>
2021-05-13 17:26:34 +0200

    Favorito JDO creado

---
+++ src/main/java/com/cliente/jdo/Favorito.java
@@ -0,0 +1,44 @@
+package com.cliente.jdo;
+
+import javax.jdo.annotations.Inheritance;
+import javax.jdo.annotations.InheritanceStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable
+@Inheritance(strategy=InheritanceStrategy.NEW_TABLE)
+public class Favorito {
+	
+	@PrimaryKey
+	protected String nickname;
+	@PrimaryKey
+	protected int idProducto;
+	
+	public Favorito(String nickname, int idProducto) {
+		super();
+		this.nickname = nickname;
+		this.idProducto = idProducto;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public int getIdProducto() {
+		return idProducto;
+	}
+
+	public void setIdProducto(int idProducto) {
+		this.idProducto = idProducto;
+	}
+
+	@Override
+	public String toString() {
+		return "Favorito [nickname=" + nickname + ", idProducto=" + idProducto + "]";
+	}
+
+}
