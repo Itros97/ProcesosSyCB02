893e75c636f6c173d59a8cbac6a33c271554c7e6
diegopresa <diego.presa@opendeusto.es>
2021-05-20 15:54:04 +0200

    Update VerCarritoVShop.java

--- src/main/java/com/cliente/ui/VerCarritoVShop.java
+++ src/main/java/com/cliente/ui/VerCarritoVShop.java
@@ -1,22 +1,18 @@
 package com.cliente.ui;
 
-import java.awt.Component;
 import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.DefaultListModel;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JTable;
-
-import com.cliente.jdo.Producto;
-
 import javax.swing.JList;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VerCarritoVShop {
 
 	private JFrame frame;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -49,41 +45,32 @@ public class VerCarritoVShop {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		frame.setTitle("VerCarritoVShop");
 		
-		JButton CerrarButton = new JButton("Cerrar");
-		CerrarButton.setBounds(10, 239, 150, 23);
-		frame.getContentPane().add(CerrarButton);
-		
-		CerrarButton.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				frame.dispose();
-			}
-		});
-		JButton ComprarButton = new JButton("Comprar");
-		ComprarButton.setBounds(274, 239, 150, 23);
-		frame.getContentPane().add(ComprarButton);
-		
-		final DefaultListModel<Producto> productListModel = new DefaultListModel<>();
-		JList<Producto> list = new JList<>(productListModel);
-		list.setBounds(10, 11, 414, 217);
+		JList list = new JList();
+		list.setBounds(10, 11, 197, 202);
 		frame.getContentPane().add(list);
 		
-		
-		for (int i = 0; i < MainVShop.carrito.size(); i++) {
-			productListModel.add(i, MainVShop.carrito.get(i));
-		}
-		
-		ComprarButton.addActionListener(new ActionListener() {
-			
-			@Override
+		JButton seleccionarUsuario = new JButton("Seleccionar Usuario");
+		seleccionarUsuario.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				CompraVShop.main(null);
-				frame.dispose();
+				//Falta insertar los usuarios de la base de datos
 			}
 		});
+		seleccionarUsuario.setBounds(10, 227, 197, 23);
+		frame.getContentPane().add(seleccionarUsuario);
+		
+		table = new JTable();
+		table.setBounds(227, 215, 197, -202);
+		frame.getContentPane().add(table);
+		
+		JButton cargarCarritos = new JButton("Cargar Carritos");
+		cargarCarritos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//Falta insertar los carritos desde la base de datos e insertarlos en la lista
+			}
+		});
+		cargarCarritos.setBounds(227, 227, 200, 23);
+		frame.getContentPane().add(cargarCarritos);
+		
 	}
 }
