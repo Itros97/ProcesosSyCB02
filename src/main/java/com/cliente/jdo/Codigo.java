7476da4398a1d554b01eef62055cb6343ccd6b1e
Itros97 <61584391+Itros97@users.noreply.github.com>
2021-05-20 11:39:27 +0200

    Codigo clase

---
+++ src/main/java/com/cliente/jdo/Codigo.java
@@ -0,0 +1,14 @@
+package com.cliente.jdo;
+
+import javax.jdo.annotations.Inheritance;
+import javax.jdo.annotations.InheritanceStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+
+@PersistenceCapable
+@Inheritance(strategy=InheritanceStrategy.NEW_TABLE)
+public class Codigo {
+	int codigo;
+	
+	
+	
+}
