42b29d887fc33ff2ba2d03e21421e2539b2a4092
Itros97 <61584391+Itros97@users.noreply.github.com>
2021-05-20 12:29:39 +0200

    Codigos BD

--- src/main/java/com/cliente/jdo/Codigo.java
+++ src/main/java/com/cliente/jdo/Codigo.java
@@ -7,8 +7,25 @@ import javax.jdo.annotations.PersistenceCapable;
 @PersistenceCapable
 @Inheritance(strategy=InheritanceStrategy.NEW_TABLE)
 public class Codigo {
-	int codigo;
-	
+	int code;
+
+	public Codigo(int code) {
+		super();
+		this.code = code;
+	}
+
+	public int getCodigo() {
+		return code;
+	}
+
+	public void setCodigo(int code) {
+		this.code = code;
+	}
+
+	@Override
+	public String toString() {
+		return "Codigo [code=" + code + "]";
+	}
 	
 	
 }
---
+++ src/main/java/com/database/CodigoBD.java
@@ -0,0 +1,56 @@
+package com.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.cliente.jdo.Codigo;
+import com.cliente.jdo.Compra;
+
+public class CodigoBD {
+	
+	 //ELIMINAR COMPRA
+    public static void eliminarCodigo(int idCodigo) {
+
+        PreparedStatement preparedStatement= null;
+        Connection con = ConexionDB.Conexion();
+
+        try {
+
+            String query = "DELETE FROM CODIGO WHERE IDCOMPRA = '" + idCodigo + "'";
+
+            preparedStatement = con.prepareStatement(query);
+
+            preparedStatement.execute();
+            preparedStatement.close();
+
+        } catch (SQLException e) {
+
+            System.out.println("No se pudo eliminar el codigo");
+            System.out.println(e);
+        }
+        
+
+    }
+    public static void insertarCompra(Codigo ncodigo)
+    {
+        PreparedStatement preparedStatement = null;
+        Connection con = ConexionDB.Conexion();
+        try {
+            String query = " INSERT INTO CODIGO (CODE)"
+                    + " VALUES (?)";
+
+            preparedStatement = con.prepareStatement(query);
+
+
+            preparedStatement.setInt(1, ncodigo.getCodigo());
+            preparedStatement.execute();
+
+            System.out.println("Insert existoso");
+
+        } catch (Exception e) {
+            System.out.println("ERROR al insertar el codigo");
+            System.out.println(e);
+        }
+    }
+}
