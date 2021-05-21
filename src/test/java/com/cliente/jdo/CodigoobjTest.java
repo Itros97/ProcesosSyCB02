bbecfe34cc260117c6d7ab7ecd0b53a0574b434b
diegopresa <diego.presa@opendeusto.es>
2021-05-21 13:25:30 +0200

    Creado el test de la clase codigoobj

--- src/test/java/com/cliente/jdo/CodigoobjTest.java
+++ src/test/java/com/cliente/jdo/CodigoobjTest.java
@@ -1,5 +1,34 @@
 package com.cliente.jdo;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class CodigoobjTest {
+
+	Codigoobj co = new Codigoobj("Codigo");
+	Codigoobj vacio = new Codigoobj(null);
+
+	@Test
+	public void testVacio() {
+		assertEquals("Codigoobj{code=''}", "Codigoobj{code=''}");
+	}
+	
+	@Test
+	public void testGetCodigo(){
+	    assertEquals("Codigo", co.getCodigo());
+	}
+
+	@Test
+	public void testSetCodigo(){
+	    co.setCodigo("Codigo2");
+	    assertEquals("Codigo2", co.getCodigo());
+	}
+	
+	@Test
+    public void testToString() {
+        String expected = "Codigo [code=" + co.code + "]";
+        assertEquals(expected, co.toString());
+    }
 
 }
